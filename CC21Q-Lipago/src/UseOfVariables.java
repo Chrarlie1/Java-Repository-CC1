a247abee4ca3ba7144ea1024cc5fe74e899725e1
Charlie <106720398+Chrarlie1@users.noreply.github.com>
2024-09-12 13:07:27 +0800

    Add files via upload
    test1

---
+++ CC21Q-Lipago/src/UseOfVariables.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author m307user
+ */
+public class UseOfVariables {
+public static void main(String[]args){
+    int age =21;
+    String name = "Charlie";
+    float decimal = (float).001;
+    boolean yesOrNo = true;
+    char middleInital = 'C';
+    System.out.println(name +" "+ middleInital + " is " + age + " this is "+ yesOrNo +
+            " they have a float of " + decimal + ".");
+
+}
+}
